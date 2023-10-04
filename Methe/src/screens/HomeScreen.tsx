9780f6236250af7749722e7d97cb9a8d1966c08f
mfreitag <mfreitag@numericable.fr>
2023-10-04 20:52:12 +0200

    basic themes ready

--- Methe/src/screens/HomeScreen.tsx
+++ Methe/src/screens/HomeScreen.tsx
@@ -1,12 +1,11 @@
-import {Text, SafeAreaView, useColorScheme} from 'react-native';
+import {Text, SafeAreaView, Switch} from 'react-native';
 
-import tw from 'twrnc';
+import tw from '../../lib/tailwind';
 
 export default function HomeScreen() {
     return (
-        <SafeAreaView style={tw`flex-1 justify-center items-center`}>
-            <Text>Welcome !</Text>
-            <Text>useColorScheme = {useColorScheme()}</Text>
+        <SafeAreaView style={tw`flex-1 justify-center items-center bg-palePeach dark:bg-darkGrayBrown`}>
+            <Text style={tw`text-black dark:text-white`}>Welcome !</Text>
         </SafeAreaView>
     );
 }
---
+++ Methe/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+    theme: {
+        extend: {
+            colors: {
+                palePeach: '#FEECCA',
+                darkGrayBrown: '#4D3E3E',
+            },
+        },
+    },
+}
