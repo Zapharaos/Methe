d78565c0e3cdeda524a47f3071929498ee80709d
mfreitag <mfreitag@numericable.fr>
2023-10-05 09:38:24 +0200

    language detection ready

--- Methe/src/screens/HomeScreen.tsx
+++ Methe/src/screens/HomeScreen.tsx
@@ -6,6 +6,9 @@ import {SelectList} from 'react-native-dropdown-select-list'
 import {StatusBar, StatusBarStyle} from "expo-status-bar";
 import {useState} from "react";
 
+import * as Localization from 'expo-localization';
+import { I18n } from 'i18n-js';
+
 export default function HomeScreen() {
     const [colorScheme, toggleColorScheme, setColorScheme] = useAppColorScheme(tw);
     const [statusBarStyle, setStatusBarStyle] = useState<StatusBarStyle>(
@@ -18,7 +21,6 @@ export default function HomeScreen() {
         {key: 'light', value: 'Light'},
         {key: 'dark', value: 'Dark'},
     ]
-
     const changeColorScheme = (key: string) => {
         if (localStorageTheme === key) return;
         localStorageTheme = key;
@@ -32,11 +34,21 @@ export default function HomeScreen() {
         }
     }
 
+    const translations = {
+        en: { welcome: 'Hello', name: 'Charlie' },
+        ja: { welcome: 'こんにちは' },
+    };
+    const i18n = new I18n(translations);
+    i18n.locale = Localization.locale;
+    i18n.enableFallback = true;
+
     return (
         <SafeAreaView style={tw`flex-1 justify-center items-center bg-palePeach dark:bg-darkGrayBrown`}>
             <StatusBar style={statusBarStyle} />
             <View style={tw`w-11/12 flex-1 justify-center items-center `}>
-                <Text style={tw`text-black dark:text-white`}>Welcome !</Text>
+                <Text style={tw`text-black dark:text-white`}>{i18n.t('welcome')} {i18n.t('name')}</Text>
+                <Text style={tw`text-black dark:text-white`}>Current locale: {i18n.locale}</Text>
+                <Text style={tw`text-black dark:text-white`}>Device locale: {Localization.getLocales()[0].languageCode}</Text>
                 <View style={tw`w-full`}>
                     <SelectList
                         setSelected={changeColorScheme}
