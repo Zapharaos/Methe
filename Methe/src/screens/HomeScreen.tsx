d37b1a3dc6cbe6e6dcacb696f65cf30216d44d22
Zapharaos <56632861+Zapharaos@users.noreply.github.com>
2023-10-06 01:14:04 +0200

    RTL ready

--- Methe/src/screens/HomeScreen.tsx
+++ Methe/src/screens/HomeScreen.tsx
@@ -1,51 +1,41 @@
-import { useAppColorScheme } from 'twrnc';
 import tw from '../../lib/tailwind';
+import rtlDetect from '../../lib/rtl-detect';
 
-import {Text, SafeAreaView, View, Appearance} from 'react-native';
+import { useAppColorScheme } from 'twrnc';
+import {Text, SafeAreaView, View, Appearance, I18nManager} from 'react-native';
 import {SelectList} from 'react-native-dropdown-select-list'
 import {StatusBar, StatusBarStyle} from "expo-status-bar";
 import {useState} from "react";
-
-import * as Localization from 'expo-localization';
 import { I18n } from 'i18n-js';
+import {getLocales} from "expo-localization";
 
 export default function HomeScreen() {
     const [colorScheme, toggleColorScheme, setColorScheme] = useAppColorScheme(tw);
     const [statusBarStyle, setStatusBarStyle] = useState<StatusBarStyle>(
         'auto',
     );
-    const [locale, setLocale] = useState(Localization.locale);
+    const [locale, setLocale] = useState(getLocales()[0]);
 
     const translations: Record<string, any> = {
-        en: { languageLabel: 'English', welcome: 'Hello'},
-        fr: { languageLabel: 'Français', welcome: 'Bonjour'},
-        ja: { languageLabel: '日本語', welcome: 'こんにちは' }
+        en: require('../../locales/en.json'),
+        fr: require('../../locales/fr.json'),
+        ar: require('../../locales/ar.json')
     };
     let localStorage = {
         theme: '',
-        language: ''
+        locale: locale
     };
 
-    /*i18n.translations = {
-        'en': require('./en.json'),
-        'nl': require('./nl.json'),
-    }*/
     const i18n = new I18n(translations);
-    i18n.locale = locale;
+    i18n.locale = locale.languageCode;
     i18n.enableFallback = true;
+    I18nManager.forceRTL(rtlDetect.isRtlLang(locale.languageCode));
 
     const themes: { key: string; value: string }[] = [
         {key: 'system', value: 'System'},
         {key: 'light', value: 'Light'},
         {key: 'dark', value: 'Dark'},
     ]
-    const languages: { key: string; value: string }[] = [
-        {key: 'system', value: 'System'},
-    ];
-    for (const key in translations) {
-        languages.push({ key, value: translations[key].languageLabel });
-    }
-
     const changeColorScheme = (key: string) => {
         if (localStorage.theme === key) return;
         localStorage.theme = key;
@@ -58,22 +48,37 @@ export default function HomeScreen() {
             setStatusBarStyle(localStorage.theme === 'dark' ? 'light' : 'dark')
         }
     }
+
+    // TODO : use a package that has all the locales
+    const languages: { key: string; value: string }[] = [];
+    for (const key in translations) {
+        languages.push({ key, value: translations[key].settings.language });
+    }
     const changeLanguage = (key: string) => {
-        if (localStorage.language === key) return;
-        if (key === "system") {
-            key = Localization.locale;
+        if (locale.languageCode === key) return;
+        // TODO : use another package that has all the locales
+        const localeUpdated = getLocales().find((l) => l.languageCode === key);
+        if (localeUpdated !== undefined) {
+            localStorage.locale = localeUpdated;
+            setLocale(localeUpdated);
+            if(I18nManager.isRTL !== rtlDetect.isRtlLang(key)) {
+                I18nManager.forceRTL(rtlDetect.isRtlLang(key));
+                console.log(rtlDetect.isRtlLang(key));
+                // TODO : popup + reload app
+            }
         }
-        localStorage.language = key;
-        setLocale(localStorage.language)
     }
 
     return (
-        <SafeAreaView style={tw`flex-1 justify-center items-center bg-palePeach dark:bg-darkGrayBrown`}>
+        <SafeAreaView style={tw`flex-1 justify-center items-center bg-palePeach dark:bg-darkGrayBrown ${I18nManager.isRTL ? 'direction-rtl' : ''}`}>
             <StatusBar style={statusBarStyle} />
-            <View style={tw`w-11/12 flex-1 justify-center items-center `}>
-                <Text style={tw`text-black dark:text-white`}>{i18n.t('welcome')}</Text>
+            <View style={tw`w-11/12 flex-1 justify-center items-center`}>
+                <Text style={tw`text-black dark:text-white`}>{i18n.t('appName')}</Text>
                 <View style={tw`w-full mt-5`}>
-                    <Text style={tw`mb-1 text-black dark:text-white`}>Language</Text>
+                    <Text style={tw`text-left text-black dark:text-white`}>{i18n.t('welcome')} {i18n.t('settings.language')}</Text>
+                </View>
+                <View style={tw`w-full mt-5`}>
+                    <Text style={tw`text-left mb-1 text-black dark:text-white`}>Language</Text>
                     <SelectList
                         setSelected={changeLanguage}
                         data={languages}
@@ -81,7 +86,7 @@ export default function HomeScreen() {
                     />
                 </View>
                 <View style={tw`w-full mt-5`}>
-                    <Text style={tw`mb-1 text-black dark:text-white`}>Theme</Text>
+                    <Text style={tw`text-left mb-1 text-black dark:text-white`}>Theme</Text>
                     <SelectList
                         setSelected={changeColorScheme}
                         data={themes}
