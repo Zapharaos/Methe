3164c8acded76c5bcc0de5e14c7417063341daa6
mfreitag <mfreitag@numericable.fr>
2023-12-09 07:01:29 +0100

    refresh favorites flatlist upon return

--- Methe/src/app/(tabs)/favourites.tsx
+++ Methe/src/app/(tabs)/favourites.tsx
@@ -6,29 +6,46 @@ import {Cocktail} from "@/src/utils/interface/CocktailInterface";
 import {getCocktailInfoById} from "@/src/utils/cocktail";
 import CocktailsFlatlist from "@/src/components/cards/CocktailsFlatlist";
 import Loader from "@/src/components/loader";
+import {useNavigation} from "expo-router";
+import {useIsFocused} from "@react-navigation/core";
 
 export default function FavouritesTab() {
 
     const {favorites} = useFavoritesContext();
     const [cocktails, setCocktails] = useState<Cocktail[]>([]);
     const [loading, setLoading] = useState(true);
+    const isFocused = useIsFocused();
+
+    const fetchCocktails = async (tempCocktails: Cocktail[]) => {
+        for (const id of favorites) {
+            try {
+                const cocktail = await getCocktailInfoById(id);
+                tempCocktails.push(cocktail);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        setCocktails(tempCocktails);
+        setLoading(false);
+    };
 
     useEffect(() => {
-        const tempCocktails :Cocktail[] = [];
-        const fetchCocktails = async () => {
-            for (const id of favorites) {
-                try {
-                    const cocktail = await getCocktailInfoById(id);
-                    tempCocktails.push(cocktail);
-                } catch (error) {
-                    console.error(error);
-                }
-            }
-            setCocktails(tempCocktails);
-            setLoading(false);
+        const fetchData = async () => {
+            await fetchCocktails([...cocktails]);
         };
-        fetchCocktails();
+        fetchData();
     }, []);
+
+
+    // Refresh the favorites on tab focus
+    useEffect(() => {
+        if (isFocused && !loading) {
+            const fetchData = async () => {
+                await fetchCocktails([]);
+            };
+            fetchData();
+        }
+    }, [isFocused]);
 
     if(loading) {
         return (
