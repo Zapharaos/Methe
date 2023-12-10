f341bddd812c050d2142a4234a9cad0515283f0c
SkelNeXus <gerling.quentin@outlook.fr>
2023-12-10 23:31:13 +0100

    Research cocktail by name or ingredient name

--- Methe/src/app/index.tsx
+++ Methe/src/app/index.tsx
@@ -1,16 +1,18 @@
-import {useEffect, useState} from "react";
-import {Button} from "react-native";
+import React, {useEffect, useState} from "react";
+import { SafeAreaView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import {Link} from "expo-router";
 import {lastValueFrom} from "rxjs";
 import {take} from "rxjs/operators";
+import {Entypo, MaterialIcons} from '@expo/vector-icons';
 import tw from "../../lib/tailwind";
 
 import {usePreferencesContext} from "../contexts/preferences/preferences";
 import BaseComponent from "../components/base";
 
 import CocktailService from "@/src/utils/services/cocktailService";
-import { Cocktail } from "@/src/utils/interface/CocktailInterface";
+import {Cocktail, FilterCocktail} from "@/src/utils/interface/CocktailInterface";
 import CocktailsContainerCards from "@/src/components/cards/CocktailsContainerCards";
+import {StringUtils} from "@/src/utils/utils";
 
 /**
  * Return type of the Api call
@@ -18,11 +20,29 @@ import CocktailsContainerCards from "@/src/components/cards/CocktailsContainerCa
 interface ApiCocktailResponse  {
     drinks: []
 }
+
+interface CocktailAPI {
+    idDrink: string,
+    strDrink: string,
+    strDrinkThumb: string,
+}
+
 export default function Index() {
     const { i18n} = usePreferencesContext();
     const likedList: bigint[] = [];
 
-    const [cocktailList, setCocktailList] = useState<Cocktail[]>([])
+    const [cocktailList, setCocktailList] = useState<Cocktail[]>([]);
+
+    const [filterCocktailList, setFilterCocktailList] = useState<Cocktail[]>();
+
+    const [filterList, setFilterList] = useState<FilterCocktail>();
+
+    /**
+     * The text written by the user
+     */
+    const [textWriteByUser, setTextWriteByUser] = useState<string>('');
+
+    const [searchByIngredient, setSearchByIngredient] = useState<boolean>(false);
 
     /**
      *  Use the Cocktail service to call the API
@@ -35,6 +55,55 @@ export default function Index() {
             console.error(err);
         }
     };
+
+    /**
+     * API call to get cocktail with cocktail name or ingredient name
+     */
+    const callResearch = async (): Promise<ApiCocktailResponse | any> => {
+        const cocktailService : CocktailService = new CocktailService();
+
+        // Nothing to do with Empty input
+        if(!StringUtils.isNullOrWhitespace(textWriteByUser))
+        {
+            // Just one character => get List all cocktails by first letter
+            if(textWriteByUser.trim().length == 1)
+            {
+                try {
+                    return await lastValueFrom(cocktailService.getCocktailByFirstLetter(textWriteByUser.trim()).pipe(take(1)));
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+            else
+            {
+                try {
+                    return await lastValueFrom(
+                        searchByIngredient ? cocktailService.getCocktailByIngredientName(textWriteByUser.trim()).pipe(take(1)) :
+                            cocktailService.getCocktailByName(textWriteByUser.trim()).pipe(take(1))
+                    );
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+        }
+    }
+
+    /**
+     * Do research with data from textWriteByUser
+     */
+    const doResearch = () => {
+        callResearch().then((result: ApiCocktailResponse) => {
+            setCocktailList([]);
+            result.drinks.map((cocktail: CocktailAPI) => {
+               const newCocktail: Cocktail = {
+                   cocktailId: BigInt(cocktail.idDrink),
+                   cocktailNames: cocktail.strDrink,
+                   cocktailImage: cocktail.strDrinkThumb,
+               };
+               setCocktailList((prevList: Cocktail[]) => [...prevList, newCocktail]);
+           });
+        });
+    }
 
     /**
      * Add a cocktail identifier into the cocktail list
@@ -73,16 +142,55 @@ export default function Index() {
         });
     }, []);
 
+    useEffect(() => {
+
+    }, [cocktailList]);
+
     return (
         <BaseComponent>
-            <Link href={'/cocktailDetail'} asChild>
-                <Button title="exemple de lien a delete"/>
-            </Link>
-            <CocktailsContainerCards
-                addIntoLikedList={addIntoLikedList}
-                cocktailList={cocktailList}
-                likedList={likedList}
-            />
+            <SafeAreaView>
+                { /* ToDo <Link href={'/cocktailDetail'} asChild>
+                    <Button title="exemple de lien a delete"/>
+                </Link> */}
+                {/* search bar */}
+                <View style={tw `mx-5 mb-5`}>
+                    <View style={tw `flex-row items-center rounded-full p-1 bg-gray-900`}>
+                        <TouchableOpacity onPress={doResearch}
+                            style={tw `rounded-full p-2 bg-stone-800 ml-1`}>
+                            <Entypo name="magnifying-glass" size={24} color="white" />
+                        </TouchableOpacity>
+
+                        <TextInput  placeholder='Search'  style={tw `p-4 flex-1 font-semibold text-white`}
+                            onChangeText={setTextWriteByUser}
+                            value={textWriteByUser}
+                            onEndEditing={doResearch}
+                        />
+                        { textWriteByUser && <TouchableOpacity
+                            style={tw `rounded-full p-2 mr-1`}
+                            onPress={() => setTextWriteByUser('')}>
+                            <MaterialIcons name="cancel" size={24} color="white" />
+                        </TouchableOpacity>}
+                    </View>
+                </View>
+
+                <View style={tw `mx-5 mb-5 flex-row items-center justify-between p-1`}>
+                    <TouchableOpacity onPress={() => setSearchByIngredient(prevState => !prevState)}
+                        style={tw `rounded-full p-2 ${!searchByIngredient ? 'bg-stone-900' : 'bg-stone-800'} ml-1`}>
+                        <Text style={tw `text-white`}> {'Search by Cocktail'} </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={() => setSearchByIngredient(prevState => !prevState)}
+                        style={tw `rounded-full p-2 ${searchByIngredient ? 'bg-stone-900' : 'bg-stone-800'} ml-1`}>
+                        <Text style={tw `text-white`}> {'Search by Ingredient'} </Text>
+                    </TouchableOpacity>
+                </View>
+
+                { cocktailList && <CocktailsContainerCards
+                    addIntoLikedList={addIntoLikedList}
+                    cocktailList={filterCocktailList ? filterCocktailList : cocktailList}
+                    likedList={likedList}
+                />}
+            </SafeAreaView>
         </BaseComponent>
     );
 }
