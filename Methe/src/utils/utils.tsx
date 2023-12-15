a89b0eaed30edd8e1d63c40e219b4a8bcb781527
SkelNeXus <gerling.quentin@outlook.fr>
2023-12-15 14:11:12 +0100

    Correction of the measure formater

--- Methe/src/utils/utils.tsx
+++ Methe/src/utils/utils.tsx
@@ -17,7 +17,34 @@ export class StringUtils {
         return template.replace(/{(\d+)}/g, (match, index) => (args[index] !== undefined ? args[index] : match));
     };
 
+    /**
+     * Return true if the string is null or whitespace
+     * @param str the string to test
+     */
     public static isNullOrWhitespace = (str: string | null | undefined): boolean => {
         return str == null || str.trim() === '';
     };
+
+    /**
+     * Return true if the string is in a fraction format
+     * @param str the string to test
+     */
+    public static isFractionFormat = (str: string) => {
+        const pattern = /^\d+\/\d+$/;
+        return pattern.test(str);
+    }
+}
+
+/**
+ * Utils class to work on numbers
+ */
+export class MathUtils {
+    public static gcd = (a: number, b: number): number => {
+        if (b === 0) {
+            return a;
+        } else {
+            return MathUtils.gcd(b, a % b);
+        }
+    };
+
 }
