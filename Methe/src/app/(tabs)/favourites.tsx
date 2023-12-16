7e8cbf2ed44b4f66342fb131eac6430c9b53f752
mfreitag <mfreitag@numericable.fr>
2023-12-16 14:37:20 +0100

    reworked the favorites

--- Methe/src/app/(tabs)/favourites.tsx
+++ Methe/src/app/(tabs)/favourites.tsx
@@ -20,58 +20,10 @@ export default function FavouritesTab() {
     // Retrieve favorites from the context
     const {favorites} = useFavoritesContext();
 
-    // State variables to manage the list of cocktails and loading state
-    const [cocktails, setCocktails] = useState<Cocktail[]>([]);
-    const [loading, setLoading] = useState(true);
-
-    // Check if the tab is currently focused
-    const isFocused = useIsFocused();
-
-    // Function to fetch details of favorite cocktails
-    const fetchCocktails = async (tempCocktails: Cocktail[]) => {
-        for (const id of favorites) {
-            try {
-                const cocktail = await getCocktailInfoById(id);
-                tempCocktails.push(cocktail);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        // Update the state with fetched cocktails and mark loading as false
-        setCocktails(tempCocktails);
-        setLoading(false);
-    };
-
-    // Initial fetch of cocktails on component mount
-    useEffect(() => {
-        const fetchData = async () => {
-            await fetchCocktails([]);
-        };
-        fetchData();
-    }, []);
-
-
-    // Refresh the favorites on tab focus
-    useEffect(() => {
-        if (isFocused && !loading) {
-            const fetchData = async () => {
-                await fetchCocktails([]);
-            };
-            fetchData();
-        }
-    }, [isFocused]);
-
-    // Render a loader while data is being fetched
-    if(loading) {
-        return (
-            <Loader/>
-        )
-    }
-
     // Render the list of cocktails once data is loaded
     return (
         <BaseComponent>
-            <CocktailsFlatlist cocktails={cocktails}/>
+            <CocktailsFlatlist cocktails={favorites}/>
         </BaseComponent>
     )
 }
